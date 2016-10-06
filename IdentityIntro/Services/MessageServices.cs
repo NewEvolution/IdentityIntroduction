ba424f67dd5a4ad1f8347d3aacad03e5d25e8e85
Ryan Tanay <ryantanay@newevolution.org>
2016-10-06 15:06:28 -0500

    Attached SendGrid for email delivery

--- IdentityIntro/Services/MessageServices.cs
+++ IdentityIntro/Services/MessageServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,28 @@ namespace IdentityIntro.Services
     // For more details see this link http://go.microsoft.com/fwlink/?LinkID=532713
     public class AuthMessageSender : IEmailSender, ISmsSender
     {
+        public AuthMessageSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; }
+
         public Task SendEmailAsync(string email, string subject, string message)
         {
             // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            var myMessage = new SendGrid.SendGridMessage();
+            myMessage.AddTo(email);
+            myMessage.From = new System.Net.Mail.MailAddress("ryantanay@newevolution.org", "Ryan Tanay");
+            myMessage.Subject = subject;
+            myMessage.Text = message;
+            myMessage.Html = message;
+            var credentials = new System.Net.NetworkCredential(
+                Options.SendGridUser,
+                Options.SendGridKey);
+            // Create web transport for sending email
+            var transportWeb = new SendGrid.Web(credentials);
+            return transportWeb.DeliverAsync(myMessage);
         }
 
         public Task SendSmsAsync(string number, string message)
