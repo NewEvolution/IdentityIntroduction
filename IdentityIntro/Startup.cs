eb81c81cf2e1814f7eb5aea3665937e429ee3169
Ryan Tanay <ryantanay@newevolution.org>
2016-10-06 13:18:54 -0500

    Added Facebook authentication and switched to unix line endings

--- IdentityIntro/Startup.cs
+++ IdentityIntro/Startup.cs
@@ -80,6 +80,11 @@ namespace IdentityIntro
             app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
 
             app.UseMvc(routes =>
             {
