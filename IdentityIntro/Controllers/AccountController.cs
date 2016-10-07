8e4832a80bb09608783fc1ccf61052c9197f4353
Ryan Tanay <ryantanay@newevolution.org>
2016-10-07 15:33:00 -0500

    Personalized email verification email body.

--- IdentityIntro/Controllers/AccountController.cs
+++ IdentityIntro/Controllers/AccountController.cs
@@ -125,8 +125,8 @@ namespace IdentityIntro.Controllers
                     // Send an email with this link
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-                    await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-                        $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
+                    await _emailSender.SendEmailAsync(model.Email, "Confirm your IdentityIntro account",
+                        $"Please confirm your account for IdentityIntro by <a href='{callbackUrl}'>clicking this link</a>.");
                     //await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
                     return RedirectToLocal(returnUrl);
